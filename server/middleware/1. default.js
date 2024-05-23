07d9cd68e7069c7b404f30bad675e6bfa5d1e821
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2024-05-23 15:50:56 +0600

    middleware updated

--- server/middleware/1. default.js
+++ server/middleware/1. default.js
@@ -1,16 +1,13 @@
 export default defineEventHandler(async ev => {
-	try {
-		if (ev.path === '/?csr=1') ev.context.nuxt = { noSSR: true }
-		if (ev.path !== '/api' && !ev.path.startsWith('/api/')) return
+	if (ev.path === '/?csr=1') ev.context.nuxt = { noSSR: true }
 
+	if (ev.path.startsWith('/api/list/')) {
+		if (mongo.connected) return
+
+		await mongo.init()
 		if (!mongo.connected) {
-			await mongo.init()
-			if (!mongo.connected) throw Error('Mongo connection failed')
+			setResponseStatus(ev, 500)
+			return send(ev)
 		}
-	} catch (e) {
-		mongo.connected = false
-		console.log(e)
-		setResponseStatus(ev, 500)
-		return send(ev)
 	}
 })

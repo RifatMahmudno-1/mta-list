0eaebab6f14f794ea5ced61216c01c4048633d8e
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2024-08-03 03:05:33 +0600

    footer link added

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="bg-theme-color-300">
-		<p class="cont text-center p-2">Made with <IconHeart class="text-rose-500" /> by Rifat Mahmud</p>
+		<p class="cont text-center p-2">Made with <IconHeart class="text-rose-500" /> by <a href="https://github.com/RifatMahmudno-1" target="_blank" class="text-rose-500 font-semibold">Rifat Mahmud</a> - {{ new Date().getFullYear() }}</p>
 	</footer>
 </template>

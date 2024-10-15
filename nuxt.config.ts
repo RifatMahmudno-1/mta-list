362ea362a4d2f5e21e755bbd5d95586a84d29304
Rifat Mahmud <rifatmahmudpc@gmail.com>
2024-10-16 03:09:00 +0600

    icons updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,11 +16,25 @@ export default defineNuxtConfig({
 			htmlAttrs: { lang: 'en' },
 			noscript: [{ children: 'Javascript is required to run this website' }],
 			meta: [
+				{ name: 'referrer', content: 'no-referrer' },
 				{ name: 'author', content: 'Rifat Mahmud' },
 				{ name: 'creator', content: 'Rifat Mahmud' },
+				{ name: 'application-name', content: 'MTA List' },
+				{ name: 'mobile-web-app-capable', content: 'yes' },
+				{ name: 'apple-mobile-web-app-capable', content: 'yes' },
+				{ name: 'apple-mobile-web-app-title', content: 'MTA List' },
+				{ name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+				{ property: 'og:site:name', content: 'MTA List' },
 				{ property: 'og:type', content: 'website' }
 			],
-			link: [{ type: 'image/png', rel: 'icon', href: '/logos/logo_256.png' }]
+			link: [
+				{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/logos/logo_16.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/logos/logo_32.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '48x48', href: '/logos/logo_48.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '64x64', href: '/logos/logo_64.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '180x180', href: '/logos/logo_180.png' },
+				{ rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/logos/logo_180.png' }
+			]
 		}
 	},
 	router: { options: { sensitive: true } },
@@ -39,16 +53,10 @@ export default defineNuxtConfig({
 			description: 'A beautiful and modern website to keep track of Movies, TV shows & seasons and animes.',
 			theme_color: '#ffffff',
 			background_color: '#ffffff',
+			display: 'standalone',
 			id: '/',
 			icons: [
-				{ src: '/logos/logo_72.png', sizes: '72x72', type: 'image/png' },
-				{ src: '/logos/logo_96.png', sizes: '96x96', type: 'image/png' },
-				{ src: '/logos/logo_128.png', sizes: '128x128', type: 'image/png' },
-				{ src: '/logos/logo_144.png', sizes: '144x144', type: 'image/png' },
-				{ src: '/logos/logo_152.png', sizes: '152x152', type: 'image/png' },
 				{ src: '/logos/logo_192.png', sizes: '192x192', type: 'image/png' },
-				{ src: '/logos/logo_256.png', sizes: '256x256', type: 'image/png' },
-				{ src: '/logos/logo_384.png', sizes: '384x384', type: 'image/png' },
 				{ src: '/logos/logo_512.png', sizes: '512x512', type: 'image/png' }
 			]
 		}
